70fd43930ac063bebd1b3411f0f01b57428eb947
gericass <bonjovi1926@yahoo.co.jp>
2018-02-19 01:23:13 +0900

    add: vim setting

--- erlang-practice/setup.sh
+++ erlang-practice/setup.sh
@@ -5,3 +5,10 @@ sudo apt-get install -y erlang
 git clone git://github.com/rebar/rebar.git
 cd rebar
 sudo ./bootstrap
+sudo git clone https://github.com/gericass/dotfiles
+cd dotfiles
+cp .vimrc ~
+mkdir -p ~/.vim/bundle
+git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
+sudo vim +NeoBundleInstall +q +q
+sudo vim +GoInstallBinaries +q +q
