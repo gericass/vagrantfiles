a21d13f0704a38a6451e0d34b2c4c4099a2866e4
gericass <bonjovi1926@yahoo.co.jp>
2018-02-24 21:51:14 +0900

    rebar -> rebar3

--- erlang-practice/setup.sh
+++ erlang-practice/setup.sh
@@ -2,7 +2,7 @@ wget https://packages.erlang-solutions.com/erlang-solutions_1.0_all.deb
 sudo dpkg -i erlang-solutions_1.0_all.deb
 sudo apt-get update
 sudo apt-get install -y erlang
-git clone git://github.com/rebar/rebar.git
+git clone https://github.com/erlang/rebar3.git
 cd rebar
 sudo ./bootstrap
 sudo git clone https://github.com/gericass/dotfiles
